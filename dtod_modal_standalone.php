40cc707b9cd75018818f1ed1f492c385cc56ba70
Harlan Bowling <harlan@paradigmcreative.com>
2020-04-21 12:57:04 -0400

    Beta 1.0.0 Release

--- dtod_modal_standalone.php
+++ dtod_modal_standalone.php
@@ -1,18 +1,3 @@
-# DTOD-Modal
-Drive-Thru on Demand Modal
-
-This script is managed by Paradigm Creative for the use of Drive-Thru on Demand customers and clients. The script creates a custom modal to feature their partnership with DTOD
-
-This script is a simplified version of the DTOD Modal Wordpress plugin also available to DTOD customers. This version uses default settings. 
-
-For questions on this script, please contact the developer at help@paradigmcreative.com
-
-------
-
-Installation Instructions
-
-1. Copy the entire script snippet provided below, including begin and end tags:
-
 <!-- Begin DTOD Modal Script -->
 <script type="text/javascript">
 	// Options Available
@@ -25,7 +10,3 @@ Installation Instructions
 <style src="https://cdn.jsdelivr.net/gh/iamharlan/dtod-modal-standalone@1/templates/css/modal.css"></style>
 <script type="text/javascript">function pc_modal(){var a=$("body");$.get("https://cdn.jsdelivr.net/gh/iamharlan/dtod-modal-standalone@1/templates/modal.html",function(t){for(var d=0;d<data.length;d++)a.append($(t))})}</script>
 <!-- End DTOD Modal Script -->
-
-2. Paste the entire script snippet into the footer of your website, just above the <body> tag.
-
-3. The snippet uses the light theme by default. You can change this in the snippet where indicated.
---
+++ templates/js/modal.js
@@ -0,0 +1,67 @@
+(function($) {  // beginning jquery function
+// Modal
+
+    // Exit intent
+    function addEvent(obj, evt, fn) {
+        if (obj.addEventListener) {
+            obj.addEventListener(evt, fn, false);
+        }
+        else if (obj.attachEvent) {
+            obj.attachEvent("on" + evt, fn);
+        }
+    }
+
+    // Apply Variables for Theme
+    $(document).ready(function() {
+        if (pc_theme == 'dark') {
+            $('#pc_modal').addClass('dark');
+            $('#pc_modal .logo').attr("src", $('#pc_modal .logo').attr("src") + "_dark.png");
+        } else if(pc_theme == 'light') {
+            $('#pc_modal').addClass('light');
+            $('#pc_modal .logo').attr("src", $('#pc_modal .logo').attr("src") + "_light.png");
+        } 
+    });
+
+
+    // Modal Deployment
+    $(document).ready(function() {
+        var visited = $.cookie('visited');
+        if (visited == 'yes') {
+            return false;
+        } else if(pc_type == 'exitintent') {
+            addEvent(document, 'mouseout', function(evt) {
+              if (evt.toElement === null && evt.relatedTarget === null && !localStorage.getItem('pc_modal_cookie')) {
+                $('#pc_modal').fadeIn();
+                $('.modalmask').fadeIn();
+                localStorage.setItem('pc_modal_cookie', 'true'); // Set the flag in localStorage
+              }
+            });
+
+        } else if(pc_type == 'timer') {
+
+            var timercount = pc_timer * 1000;
+
+            setTimeout(function() {
+                $('#pc_modal').fadeIn();
+                $('.modalmask').fadeIn();
+                localStorage.setItem('pc_modal_cookie', 'true'); // Set the flag in localStorage
+            }, timercount);            
+        }
+        // Cookie Expires every 5 Days
+        $.cookie('visited', 'yes', { expires: 5, path: '/' });
+
+        // Test if session cookie is enabled
+        // if("pc_modal_cookie" in localStorage){
+        //    alert('YES, Cookie is in LocalStorage');
+        // } else {
+        //    alert('NO, Cookie is not in LocalStorage');
+        // }
+    });
+
+    // Closing the Popup Box
+    $('.close_modal, .modalmask').click(function() {
+      $('#pc_modal').fadeOut();
+      $('.modalmask').fadeOut();
+    });
+
+})( jQuery );  // End of wordpress function
